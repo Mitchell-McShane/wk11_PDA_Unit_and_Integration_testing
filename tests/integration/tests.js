b1c793652e698981cfbf75161e0698511a528f0b
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2021-01-22 17:41:02 +0000

    fourth UI Integration test passed

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -45,4 +45,33 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('1');
   })
 
+  it('should be able to output negative numbers', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-7');
+  })
+
+  it('should be able to output decimal numbers', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number6')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1.5');
+  })
+
+  it('should be able to output large numbers', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number4')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#operator_multiply')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('6553600000000');
+  })
+
 });
